4bf2485bfacd49106a0c38fbb0fd61b132829a3f
uk4z <118286479+uk4z@users.noreply.github.com>
2023-03-07 15:00:22 +0100

    Update test_s1q5_min_distance.py

--- tests/test_s1q5_min_distance.py
+++ tests/test_s1q5_min_distance.py
@@ -1,1 +1,14 @@
-# À compléter
+import sys 
+sys.path.append("D:\Coding files\Delivery network")
+
+import unittest 
+from graph import graph_from_file
+
+class Test_GraphLoading(unittest.TestCase):
+    def test_network4(self):
+        g = graph_from_file("input/network.04.in")
+        self.assertEqual(g.get_path_with_power(1,4,11), [1, 4])
+        self.assertEqual(g.get_path_with_power(1,4,7), [1,2,3,4])
+
+if __name__ == '__main__':
+    unittest.main()
