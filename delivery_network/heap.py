214aeebfd989c5ea86495729058e56d90e616f59
uk4z <118286479+uk4z@users.noreply.github.com>
2023-03-23 10:05:19 +0100

    Update heap.py
    More readable version of the code

--- delivery_network/heap.py
+++ delivery_network/heap.py
@@ -5,206 +5,215 @@ import math
 class FibonacciHeapNode:
     def __init__(self, wrap, key):
         self.wrap = wrap
-        self.parent = None # Parent pointer
-        self.child = None # Child pointer
-        self.left = self.right = self # Pointer to the node on the left and on the right
-        self.key = key # Value of the node
-        self.degree = 0 # Degree of the node
-        self.mark = False # Boolean mark of the node
+        self.key = key
+        self.parent = None
+        self.child = None 
+        self.left = self.right = self
+        self.degree = 0 
+        self.mark = False 
     
-    # Linking the heap nodes in parent child relationship
-
     def suppress_neighbors(self):
         self.left.right = self.right
         self.right.left = self.left 
         self.right = self.left = self
     
-    def add_as_neighbor(self, node):
+    def link_to_the_left(self, node):
+        if self.right != self:
+            raise Exception("The node has at least one neighbor.")
+        
         node.left.right = self 
         self.left = node.left 
         self.right = node
         node.left = self 
 
     def link_as_child(self, parent):
-        if self == parent:
-            return 
-        self.suppress_neighbors()
+        if self != parent: 
+            self.suppress_neighbors()
 
-        if not parent.child:
-            parent.child = self
-        else:
-            child = parent.child
-            self.add_as_neighbor(child)
+            if parent.child is not None:
+                child = parent.child
+                self.link_to_the_left(child)
+            else:
+                parent.child = self
 
-        self.parent = parent
-        parent.degree += 1
+            self.parent = parent
+            parent.degree += 1
  
 
 class FibonacciHeap:
     def __init__(self):
         self.min_node = None
         self.nb_nodes = 0
-        
-    def is_empty(self):
-        return self.nb_nodes == 0 
     
+    def update_min_node(self, node):
+        if node.key < self.min_node.key:
+            self.min_node = node
+
     def insertion(self, wrap, key):
         new_node = FibonacciHeapNode(wrap, key)
 
-        if not self.min_node:
-            self.min_node = new_node
+        if self.min_node is not None:
+            new_node.link_to_the_left(self.min_node)
+            self.update_min_node(new_node)
+
         else:
-            new_node.add_as_neighbor(self.min_node)
-            if (new_node.key < self.min_node.key):
-                self.min_node = new_node
-
+            self.min_node = new_node
+            
         self.nb_nodes += 1
-
-    def get_min_wrap(self):
-        if not self.min_node:
-            return None
-        return self.min_node.wrap
     
-    def have_node(self, wrap_node):
-        if not self.min_node:
+    def have_wrap(self, wrap):
+        if self.min_node is None:
             return False
-        return get_node_by_wrap(self.min_node, wrap_node) is not None
-    
-    def get_node(self, wrap):
-        if not self.min_node:
-            return None 
-        return get_node_by_wrap(self.min_node, wrap)
-
         
-    # Consolidating the heap
-    def consolidate(self):
-        max_degree = 2 * int(math.log2(self.nb_nodes)) + 1
-        arr = [None] * (max_degree + 1)
+        return dfs_node(self.min_node, wrap) is not None
+    
+    def _get_node_by_wrap(self, wrap):
+        if self.min_node is None:
+            return None 
+        
+        return dfs_node(self.min_node, wrap)
+
+    def _add_child_to_root_list(self):
+        if self.min_node.child is not None:
+                child = self.min_node.child
+
+                while True:
+                    neighbor = child.right
+
+                    child.suppress_neighbors()
+                    child.link_to_the_left(self.min_node)
+                    child.parent = None
+
+                    child = neighbor
+
+                    if neighbor == self.min_node.child:
+                        break
+
+                self.min_node.child = None
+
+    def _extract_only_node(self):
+        extracted_node = self.min_node
+        self.min_node = None
+        self.nb_nodes = 0  
+
+        return extracted_node.wrap
+
+    def extract_min(self):
+        if self.min_node is not None:
+            self._add_child_to_root_list()
+
+            if self.min_node == self.min_node.right:
+                return self._extract_only_node()
+            
+            extracted_node = self.min_node
+            self.min_node = self.min_node.right
+            extracted_node.suppress_neighbors()
+
+            self.nb_nodes -= 1
+            self.consolidate()
+            
+            return extracted_node.wrap
+    
+    def _link_same_degree_nodes(self, root_list):
         node = self.min_node
         while True:
             degree = node.degree
-            while arr[degree]:
-                neighbour = arr[degree]
+
+            while root_list[degree]:
+                neighbour = root_list[degree]
+
                 if neighbour.key < node.key:
                     neighbour, node = node, neighbour
+
                 if neighbour == self.min_node:
                     self.min_node = node
+
                 neighbour.link_as_child(node)
+
                 if node.right == node:
                     self.min_node = node
-                arr[degree] = None
+
+                root_list[degree] = None
                 degree += 1
 
-            arr[degree] = node
+            root_list[degree] = node
             node = node.right
+
             if node == self.min_node:
                 break
 
+    def _set_min_node_from_root_list(self, root_list):
         self.min_node = None
-        for node in arr:
+        for node in root_list:
             if node:
-                if not self.min_node:
+                if self.min_node is None:
                     self.min_node = node
                 else:
                     node.suppress_neighbors()
-                    node.add_as_neighbor(self.min_node)
+                    node.link_to_the_left(self.min_node)
                     
-                    if node.key < self.min_node.key:
-                        self.min_node = node
-             
+                    self.update_min_node(node)
 
-    # Function to extract self.min_nodemum node in the heap
-    def extract_min(self):
-        if not self.min_node:
-            return None 
+    def consolidate(self):
+        MAX_DEGREE = 2 * int(math.log2(self.nb_nodes)) + 1
+        root_list = [None] * (MAX_DEGREE + 1)
 
-        min_node = self.min_node
-        output = self.get_min_wrap()
+        self._link_same_degree_nodes(root_list)
+        self._set_min_node_from_root_list(root_list)
 
-        if min_node.child:
-            child = min_node.child
-            while True:
-                neighbor = child.right
-                child.add_as_neighbor(min_node)
-                child.parent = None
-                child = neighbor
-
-                if neighbor == min_node.child:
-                    break
-            min_node.child = None
-
-        if min_node == min_node.right:
-            self.min_node = None
-            self.nb_nodes = 0   
-            return output
-        self.min_node = self.min_node.right
-
-        min_node.suppress_neighbors()
-        self.nb_nodes -= 1
-        self.consolidate()
         
 
-        return output
-
-    # cutting a node in the heap to be placed in the root list
-    def cut(self, node, parent):
+    def _cut(self, node, parent):
         if node == parent.child:
-            parent.child = node.right if node.right != node else None
+            parent.child = node.right if (node.right != node) else None
 
-        node.suppress_neighbors()
-        parent.degree -= 1
-        node.parent = None
-        node.add_as_neighbor(self.min_node)
         node.mark = False 
+        node.parent = None
+        node.suppress_neighbors()
+        node.link_to_the_left(self.min_node)
+        
+        parent.degree -= 1
 
-    # Recursive cascade cutting function
-    def cascade_cut(self, node):
+    def _cascade_cut(self, node):
         parent = node.parent
-        if not parent:
-            return 
-        if not node.mark :
-            node.mark = True
-        else:
-            self.cut(node, parent)
-            self.cascade_cut(parent)
 
-    # Function to decrease the value of a node in the heap
-    def Decrease_key(self, node_wrap, new_key):
-        if not self.min_node:
-            raise Exception("The Heap is Empty")
+        if parent is not None:
+            if not node.mark :
+                node.mark = True
 
-        if not self.have_node(node_wrap):
-            raise Exception("Node is not in the Heap")
+            else:
+                self._cut(node, parent)
+                self._cascade_cut(parent)
 
-        node = self.get_node(node_wrap)
+    def decrease_key(self, node_wrap, new_key):
+        node = self._get_node_by_wrap(node_wrap)
         node.key = new_key
 
         parent = node.parent
         
-        if parent and node.key < parent.key:
-            self.cut(node, parent)
-            self.cascade_cut(parent)
+        if (parent is not None) and node.key < parent.key:
+            self._cut(node, parent)
+            self._cascade_cut(parent)
 
-        if node.key < self.min_node.key:
-            self.min_node = node
+        self.update_min_node(node)
 
-def get_node_by_wrap(node, wrap):
+def dfs_node(node, wrap):
     stack = [node]
     visited = set()
 
     while stack:
-        current = stack.pop()
-        if current in visited:
-            continue
-        visited.add(current)
+        current_node = stack.pop()
 
-        if current.wrap == wrap:
-            return current
+        if current_node not in visited:
+            visited.add(current_node)
 
-        if current.child:
-            stack.append(current.child)
+            if current_node.wrap == wrap:
+                return current_node
 
-        if current.right and current.right != node:
-            stack.append(current.right)
+            if current_node.child:
+                stack.append(current_node.child)
+
+            if current_node.right != current_node:
+                stack.append(current_node.right)
 
     return None
