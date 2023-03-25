8d39b477b6c8c165796505ca6fd90bb2d7fef4f2
uk4z <118286479+uk4z@users.noreply.github.com>
2023-03-25 17:48:07 +0100

    Update graph_rewriting.py
    Add a __str__ method to the graph

--- delivery_network/graph_rewriting.py
+++ delivery_network/graph_rewriting.py
@@ -3,58 +3,74 @@ sys.path.append("D:\Coding files\Delivery network\Fibonacci Heap")
 from fibonacci_heap import FibonacciHeap
 import collections
 
+class GraphEdge:
+    def __init__(self, node1=None, node2=None, power=0, distance=1):
+        self.node1 = node1
+        self.node2 = node2
+        self.power = power
+        self.distance = distance
+    
+    def __str__(self):
+        source = self.node1.value
+        destination = self.node2.value
+
+        return f"{source} --- {destination}"
+    
+    def find_neighbor_in_edge(self, node):
+            if self.node1 != node:
+                return self.node1
+            
+            else: 
+                return self.node2
+
+class GraphNode:
+    def __init__(self, value, neighbors=None):
+        self.value = value
+        self.neighbors = neighbors or {}
+    
+    def is_connected(self, target):
+        visited = set()
+        stack = [self]
+
+        while stack:
+            node = stack.pop()
+
+            if node == target:
+                return True
+            
+            visited.add(node)
+                
+            for neighbor in node.neighbors.keys():
+                if neighbor not in visited:
+                    stack.append(neighbor)
+
+        return False
+
+    def set_neighbor(self, node, edge):
+        if node not in self.neighbors:
+            self.neighbors[node] = [edge]
+
+        else:
+            self.neighbors[node].append(edge)
+         
+           
 class Graph:
     def __init__(self):
         self.nodes = {}
         self.edges = []
 
-    class Edge:
-        def __init__(self, node1=None, node2=None, power=0, distance=1):
-            self.node1 = node1
-            self.node2 = node2
-            self.power = power
-            self.distance = distance
+    def __str__(self):
+        if not self.nodes.keys():
+            output = "The graph is empty."
+
+        else:
+            output = f"The graph has {len(self.nodes)} nodes and {len(self.edges)} edges.\n"
+            for edge in self.edges:
+                source = edge.node1.value
+                destination = edge.node2.value
+                output += f"{source} --- {destination}\n"
         
-        def __str__(self):
-            source = self.node1.value
-            destination = self.node2.value
-
-            return f"{source} --- {destination}"
-        
-        def find_neighbor_in_edge(self, node):
-                if self.node1 != node:
-                    return self.node1
-                
-                else: 
-                    return self.node2
-                
-    class Node:
-        def __init__(self, value, neighbors=None, edges=None):
-            self.value = value
-            self.neighbors = neighbors or []
-            self.edges = edges or []
-        
-        def is_connected(self, target):
-            visited = set()
-            stack = [self]
-
-            while stack:
-                node = stack.pop()
-
-                if node == target:
-                    return True
-                
-                visited.add(node)
-
-                for neighbor in node.neighbors:
-                    if neighbor not in visited:
-                        stack.append(neighbor)
-
-            return False
-
-        def set_neighbor(self, node):
-            if node not in self.neighbors:
-                self.neighbors.append(node) 
+        return output
 
     def get_node_by_value(self, value):
         return self.nodes[value] 
@@ -101,20 +117,21 @@ def dijkstra(source, peres, distances, already_processed):
         visit_neighbors(node, heap, peres, distances, already_processed)
 
 def visit_neighbors(node, heap, peres, distances, already_processed):
-        for edge in node.edges:
-            neighbor = edge.find_neighbor_in_edge(node)
+        for edges in node.neighbors.values():
+            for edge in edges:
+                neighbor = edge.find_neighbor_in_edge(node)
 
-            if (not already_processed[neighbor]
-                and (distances[neighbor] == -1 
-                     or distances[neighbor] >= distances[node] + edge.distance)):
-                peres[neighbor] = node
-                distances[neighbor] = distances[node] + edge.distance
+                if (not already_processed[neighbor]
+                    and (distances[neighbor] == -1 
+                        or distances[neighbor] >= distances[node] + edge.distance)):
+                    peres[neighbor] = node
+                    distances[neighbor] = distances[node] + edge.distance
 
-                if heap.have_wrap(neighbor):
-                    heap.decrease_key(neighbor, distances[neighbor])
-                    
-                else:
-                    heap.insertion(neighbor, distances[neighbor])
+                    if heap.have_wrap(neighbor):
+                        heap.decrease_key(neighbor, distances[neighbor])
+                        
+                    else:
+                        heap.insertion(neighbor, distances[neighbor])
             
 def get_path_from_peres(source, destination, peres):
     path = collections.deque()
