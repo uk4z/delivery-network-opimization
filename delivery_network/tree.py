9245845cbc6ae8f565e691dea3331ead28f0c58c
uk4z <118286479+uk4z@users.noreply.github.com>
2023-04-03 11:01:22 +0200

    Update tree.py

--- delivery_network/tree.py
+++ delivery_network/tree.py
@@ -1,20 +1,29 @@
+from scipy.stats import bernoulli
+
 class TreeNode:
-    def __init__(self, value, power, parent=None):
+    def __init__(self, value, power, distance, parent=None):
         self.parent = parent
         self.children = []
         self.value = value
         self.power = power
-
+        self.distance = distance
+        self.broke = False
+    
+    def broke_with_probability(self, epsilon):
+        value = bernoulli(epsilon)
+        if  value == 1:
+            self.broke = True
 
 class Tree:
     def __init__(self, root):
         self.root = root
         self.nodes = {}
 
-    def find_min_power(self, value1, value2):
+    def find_min_power(self, value1, value2, gas_price, broke_routes):
         if value1 == value2:
-            return -1
+            return 0, 0
         
+        route_distance = 0
         node1 = self.nodes[value1]
         node2 = self.nodes[value2]
         lca = lowest_common_ancestor(node1, node2)
@@ -22,20 +31,31 @@ class Tree:
 
         node = node1
         while node != lca:
+            if node.broke and node!= self.root and broke_routes:
+                return None, 0
+            
             min_power = max(min_power, node.power)
+            route_distance += node.distance
             node = node.parent 
         
         node = node2
         while node != lca:
+            if node.broke and node!= self.root and broke_routes:
+                return None, 0
+            
             min_power = max(min_power, node.power)
+            route_distance += node.distance
             node = node.parent
+
+        route_cost = gas_price * route_distance
+
+        return  min_power, route_cost
     
-        return  min_power
-    
-    def find_min_power_path(self, value1, value2):
+    def find_min_power_path(self, value1, value2, gas_price, broke_routes):
         if value1 == value2:
-            return []
+            return [value1], 0
         
+        route_distance = 0
         node1 = self.nodes[value1]
         node2 = self.nodes[value2]
         lca = lowest_common_ancestor(node1, node2)
@@ -43,20 +63,49 @@ class Tree:
         first_part = []
         node = node1
         while node != lca:
+            if node.broke and node!= self.root and broke_routes:
+                return None, 0
+            
             first_part.append(node.value)
+            route_distance += node.distance
             node = node.parent 
         first_part.append(lca.value)
 
         second_part = []        
         node = node2
         while node != lca:
+            if node.broke and node!= self.root and  broke_routes:
+                return None, 0
+            
             second_part.append(node.value)
+            route_distance += node.distance
             node = node.parent
     
         path = first_part + second_part[::-1]
 
-        return path 
+        route_cost = gas_price * route_distance
+        return path, route_cost
 
+    def nb_edges_in_route(self, value1, value2):
+        if value1 == value2:
+            return 0
+        
+        nb_edges = 0
+        node1 = self.nodes[value1]
+        node2 = self.nodes[value2]
+        lca = lowest_common_ancestor(node1, node2)
+
+        node = node1
+        while node != lca:
+            nb_edges += 1  
+            node = node.parent 
+        
+        node = node2
+        while node != lca:
+            nb_edges += 1
+            node = node.parent
+
+        return nb_edges
 
 def lowest_common_ancestor(node1, node2):
     ancestors = set()
