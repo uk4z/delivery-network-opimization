45b9437c13dc9211fcb0b85fd91e4e52f9e78323
uk4z <118286479+uk4z@users.noreply.github.com>
2023-04-03 15:52:30 +0200

    Update tree.py
    New version with more concision

--- delivery_network/tree.py
+++ delivery_network/tree.py
@@ -2,11 +2,11 @@ from scipy.stats import bernoulli
 
 class TreeNode:
     def __init__(self, value, power, distance, parent=None):
-        self.parent = parent
-        self.children = []
         self.value = value
         self.power = power
         self.distance = distance
+        self.parent = parent
+        self.children = []
         self.broke = False
     
     def broke_with_probability(self, epsilon):
@@ -17,95 +17,61 @@ class TreeNode:
 class Tree:
     def __init__(self, root):
         self.root = root
+        self.gas_price = 0
         self.nodes = {}
 
-    def find_min_power(self, value1, value2, gas_price, broke_routes):
-        if value1 == value2:
-            return 0, 0
-        
-        route_distance = 0
+    def route_characteristics(self, value1, value2, broke=True, path=False, power=False, distance=False, cost=False, number_of_edges=False):
         node1 = self.nodes[value1]
         node2 = self.nodes[value2]
         lca = lowest_common_ancestor(node1, node2)
-        min_power = 0
 
-        node = node1
-        while node != lca:
-            if node.broke and node!= self.root and broke_routes:
-                return None, 0
-            
-            min_power = max(min_power, node.power)
-            route_distance += node.distance
-            node = node.parent 
+        first_part, power1_value, distance1_value = self.characteristics_until_lca(node1, lca, broke)
+        second_part, power2_value, distance2_value = self.characteristics_until_lca(node2, lca, broke)
+
+        path_nodes = first_part + [lca.value] + second_part[::-1]
+        power_value = power1_value + power2_value
+        distance_value = distance1_value + distance2_value
+        route_cost = distance_value * self.gas_price
+        nb_edges = len(path_nodes)-1
         
-        node = node2
-        while node != lca:
-            if node.broke and node!= self.root and broke_routes:
-                return None, 0
+
+        output = {"path" : [path_nodes, path], 
+                  "power" : [power_value, power], 
+                  "distance" : [distance_value, distance], 
+                  "edges" : [nb_edges, number_of_edges],
+                  "cost" : [route_cost, cost]}
+
+        return [output[key][0] for key in ["path", "power", "distance", "cost", "edges"] if output[key][1]]
+
+    def characteristics_until_lca(self, node, lca, broke=True):
+        path = []
+        power = 0
+        distance = 0
+        nodes = self.iterate_from_node_to_lca(node, lca)
+
+        for node in nodes:
+            if node.broke and node != self.root and broke:
+                return []
             
-            min_power = max(min_power, node.power)
-            route_distance += node.distance
-            node = node.parent
-
-        route_cost = gas_price * route_distance
-
-        return  min_power, route_cost
+            if node != lca:
+                power = max(power, node.power)
+                distance += node.distance
+                path.append(node.value)
+        
+        return path, power, distance
     
-    def find_min_power_path(self, value1, value2, gas_price, broke_routes):
-        if value1 == value2:
-            return [value1], 0
-        
-        route_distance = 0
-        node1 = self.nodes[value1]
-        node2 = self.nodes[value2]
-        lca = lowest_common_ancestor(node1, node2)
+    def iterate_from_node_to_lca(self, start_node, lca):
+        node = start_node
 
-        first_part = []
-        node = node1
         while node != lca:
-            if node.broke and node!= self.root and broke_routes:
-                return None, 0
+            if node.broke and node!= self.root:
+                return None
             
-            first_part.append(node.value)
-            route_distance += node.distance
-            node = node.parent 
-        first_part.append(lca.value)
-
-        second_part = []        
-        node = node2
-        while node != lca:
-            if node.broke and node!= self.root and  broke_routes:
-                return None, 0
-            
-            second_part.append(node.value)
-            route_distance += node.distance
-            node = node.parent
-    
-        path = first_part + second_part[::-1]
-
-        route_cost = gas_price * route_distance
-        return path, route_cost
-
-    def nb_edges_in_route(self, value1, value2):
-        if value1 == value2:
-            return 0
-        
-        nb_edges = 0
-        node1 = self.nodes[value1]
-        node2 = self.nodes[value2]
-        lca = lowest_common_ancestor(node1, node2)
-
-        node = node1
-        while node != lca:
-            nb_edges += 1  
-            node = node.parent 
-        
-        node = node2
-        while node != lca:
-            nb_edges += 1
+            yield node
             node = node.parent
 
-        return nb_edges
+        yield lca
+
 
 def lowest_common_ancestor(node1, node2):
     ancestors = set()
@@ -117,6 +83,9 @@ def lowest_common_ancestor(node1, node2):
     while node2:
         if node2 in ancestors:
             return node2
+        
         node2 = node2.parent
 
     return None
+
+
